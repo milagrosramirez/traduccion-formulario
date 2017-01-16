5a0143b13f27e91e0e441024f54410fc5a86e814
Maribel <bell.chani41@gmail.com>
2017-01-16 12:47:11 -0500

    subo archivos

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+	document.getElementById('form-signin-heading').innerHTML="Por favor inicia sesión";
+	document.getElementById("inputEmail").placeholder="Ingresa correo clectronico";
+	document.getElementById('inputPassword').placeholder="Contraseña";
+	document.querySelector('span').innerHTML="Recordar datos";
+	document.querySelector('button').innerHTML="Iniciar Sesión";
+
